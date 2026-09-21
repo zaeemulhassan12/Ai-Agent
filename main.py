6b8bd30773bff8134d4ffdd5e1837a2d9bc9ea8e
zaeemulhassan12 <zaeemulhassan819.com>
2026-09-22 02:23:43 +0500

    Ai agent Model

---
+++ main.py
@@ -0,0 +1,60 @@
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv(".env.local")
+
+# print("LIVEKIT_URL:", os.getenv("LIVEKIT_URL"))
+# print("LIVEKIT_API_KEY:", os.getenv("LIVEKIT_API_KEY"))
+# print("SECRET LOADED:", bool(os.getenv("LIVEKIT_API_SECRET")))
+
+from dotenv import load_dotenv
+
+from livekit import agents
+from livekit.agents import AgentServer, AgentSession, Agent, inference, room_io, TurnHandlingOptions
+from livekit.plugins import ai_coustics
+
+load_dotenv(".env.local")
+
+
+class Assistant(Agent):
+    def __init__(self) -> None:
+        super().__init__(
+            instructions="""You are a helpful voice AI assistant.
+            You eagerly assist users with their questions by providing information from your extensive knowledge.
+            Your responses are concise, to the point, and without any complex formatting or punctuation including emojis, asterisks, or other symbols.
+            You are curious, friendly, and have a sense of humor.""",
+        )
+
+server = AgentServer()
+
+@server.rtc_session(agent_name="my-agent")
+async def my_agent(ctx: agents.JobContext):
+    session = AgentSession(
+        stt=inference.STT(model="deepgram/nova-3", language="multi"),
+        llm=inference.LLM(model="google/gemma-4-31b-it"),
+        tts=inference.TTS(
+            model="inworld/inworld-tts-2",
+            voice="Ashley",
+        ),
+        turn_handling=TurnHandlingOptions(
+            turn_detection=inference.TurnDetector(),
+        ),
+    )
+
+    await session.start(
+        room=ctx.room,
+        agent=Assistant(),
+        room_options=room_io.RoomOptions(
+            audio_input=room_io.AudioInputOptions(
+                noise_cancellation=ai_coustics.audio_enhancement(model=ai_coustics.EnhancerModel.QUAIL_VF_S),
+            ),
+        ),
+    )
+
+    await session.generate_reply(
+        instructions="Greet the user and offer your assistance."
+    )
+
+
+if __name__ == "__main__":
+    agents.cli.run_app(server)
